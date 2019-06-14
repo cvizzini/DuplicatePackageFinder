8b9a85c813a18eb0121aeb917336311a897e485c
Claudio <claudio@iplatform.co.za>
2019-06-14 17:36:38 +0200

    feat: initial commit

---
+++ DuplicatePackageFinder/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace DuplicatePackageFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            Console.WriteLine("Finding Duplicate Package References");
+
+            //Configure the path and search pattern
+            var path = config["folderPath"];
+            var searchPattern = config["searchFilePattern"];
+            Console.WriteLine($"Looking for files with pattern: {searchPattern} in path: {path}");
+
+            //Collect all files matching the search pattern in the folder path provided
+            var files = Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories);
+            Console.WriteLine($"{files.Count()} files found...");
+
+            //Configure the regex
+            Regex regex = GetRegex();
+            string tempLineValue;
+            foreach (var file in files)
+            {
+                var referenceLines = new List<string>();
+                using (StreamReader inputReader = new StreamReader(file))
+                {
+                    while (null != (tempLineValue = inputReader.ReadLine()))
+                    {
+                        //Get all package lines
+                        if (regex.Match(tempLineValue).Success)
+                        {
+                            //store the line without the version number
+                            var output = Regex.Replace(tempLineValue, @"[\d-]", string.Empty);
+
+                            referenceLines.Add(output);
+                        }
+                    }
+                }
+
+                // Group by packages that have more than one of the same reference
+                var query = referenceLines.GroupBy(x => x)
+                              .Where(g => g.Count() > 1)
+                              .Select(y => new { Element = y.Key, Counter = y.Count() })
+                              .ToList();
+
+
+                if (query.Any())
+                {
+                    //Output duplicate lines to console
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(file);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    foreach (var duplicate in query)
+                    {
+                        Console.WriteLine($"{duplicate}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static Regex GetRegex()
+        {
+            string re3 = "(PackageReference)";
+
+            Regex r = new Regex(re3, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            return r;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # DuplicatePackageFinder
 Finds duplicate packages references in all *.*proj (C# and F# project files) in a given folder.
+
+
+
+# RabbitMQPurger
+Remove all messages in all [Rabbit MQ](https://www.rabbitmq.com/) Queues
+
+----
+## Usage
+
+Set the connection settings in the appsettings.config
+
+    {
+		  "folderPath": "D:\\Source\\Test",
+		  "searchFilePattern": "*sproj"
+	}
+
+Build and Run
+
+## Example Expected Output
+
+    Finding Duplicate Package References
+	Looking for files with pattern: *sproj in path: D:\Source\Test
+	D:\Source\Test\Api.Core\Api.Core.csproj
+	{ Element =     <PackageReference Include="Microsoft.VisualStudio.Web.BrowserLink" Version=".." />, Counter = 2 }
+	{ Element =     <PackageReference Include="Polly" Version=".." />, Counter = 2 }
+
+	Done
+
+
